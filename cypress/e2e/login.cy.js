abbc8eef0d0737e3db541a0c2d07635bd2f01c9c
Gabriel Mai <gabriel.mai@belluno.digital>
2024-08-19 12:51:44 -0300

    feat: Add API login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress" />
+
+describe('Teste de API - Login', () => {
+
+  it('Deve realizar login com sucesso', () => {
+
+    cy.request({
+      method: 'POST',
+      url: 'login',
+      body: {
+        email: 'fulano@qa.com',
+        password: 'teste'
+      },
+    }).should((response) => {
+      expect(response.status).to.eq(200)
+        expect(response.body.message).to.equal('Login realizado com sucesso')
+      expect(response.body).to.have.property('authorization')
+    })
+
   })
+
 })
